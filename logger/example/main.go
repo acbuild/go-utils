4489a7abd021274400223e2b829461ab5fb659bb
acbuild <acraig94@gmail.com>
2025-05-23 11:18:02 +0100

    add option for file output

--- logger/example/main.go
+++ logger/example/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/acbuild/go-utils/logger"
 )
@@ -10,9 +9,12 @@ import (
 func main() {
 	lgr := logger.New(logger.LevelInfo, logger.WithOutput(os.Stdout), logger.WithPrefixDateTime())
 
+	// Example with logging to file
+	//lgr := logger.New(logger.LevelInfo, logger.WithPrefixDateTime(), logger.WithFileOutput("app.log"))
+	//defer lgr.Close()
+
 	lgr.Infof("Hello %s", "world")
 	lgr.Debugf("I am a debug message")
 	lgr.Errorf("I am an error message")
 	lgr.Warnf("I am warning message")
-	lgr.Infof("I am an info messag %s", time.Now())
 }
